d7265e116133d54cf4533924572697bf68477432
Jake Rios <jakerios01@gmail.com>
2024-02-13 14:46:07 -0500

    reorganized navbar and setup user dropdown menu

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -8,29 +8,31 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function Header() {
   return (
-    <Navbar bg="primary" data-bs-theme="dark" expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">Campus Link</Navbar.Brand>
+        <Navbar.Brand href="/pages/home">
+            <img
+              src="/app/favicon.ico"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />Campus Link</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/pages/home">Home</Nav.Link>
-            <Nav.Link href="/pages/logIn">Log In</Nav.Link>
-            <Nav.Link href="/pages/register">Register</Nav.Link>
-            <Nav.Link href="/pages/professors">Professors</Nav.Link>
-            <Nav.Link href="/pages/tutors">Tutors</Nav.Link>
-            <Nav.Link href="/pages/users">Users</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown title="Users" id="basic-nav-dropdown">
+              <NavDropdown.Item href="/pages/students">Students</NavDropdown.Item>
+              <NavDropdown.Item href="/pages/professors">Professors</NavDropdown.Item>
+              <NavDropdown.Item href="/pages/tutors">Tutors</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">
                 Separated link
               </NavDropdown.Item>
             </NavDropdown>
+            <Nav.Link href="/pages/logIn">Log In</Nav.Link>
+            <Nav.Link href="/pages/register">Register</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
