a63d823079512d41ccfd1817e4c8f7cfd67fa3ea
sarkr72 <sarkrc@farmingdale.edu>
2024-03-07 20:49:45 -0500

    made create session2

---
+++ app/pages/createTutoringSession/page.jsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+import TimeSlotPicker from "../../components/TimeSlotPicker";
+import styless from "../../../styles/timeSlot.css";
+function page() {
+  return (
+    <div>
+      <TimeSlotPicker />
+    </div>
+  );
+}
+
+export default page;
