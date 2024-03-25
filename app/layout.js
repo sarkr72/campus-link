adb82a1f7fdf175e053d811b8aa3fde59f9223ec
Jake Rios <jakerios01@gmail.com>
2024-03-25 11:54:09 -0400

    testing new fonts

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 import { Inter } from "next/font/google";
-// import "./globals.css";
+import "./globals.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header'
 import "react-toastify/dist/ReactToastify.css";
