8e30bdabff3f9cd5b4ada814b172b4544a9ec4ae
sarkr72 <sarkrc@farmingdale.edu>
2024-04-01 09:14:53 -0400

    deploying to vercel

--- app/utils/firebaseAdmin.js
+++ app/utils/firebaseAdmin.js
@@ -1,14 +1,14 @@
-import { app } from './firebase';
+import { auth } from './firebase';
 import admin from 'firebase-admin';
-import key from '@/key';
+// import key from '@/key';
 // const serviceAccount = require('../key');
 
 // Initialize Firebase Admin SDK
 if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(key),
+  // admin.initializeApp({
+    // credential: admin.credential.cert(key),
     // databaseURL: 'https://your-project-id.firebaseio.com'
-  });
+  // });
 }
 
 export { admin };
