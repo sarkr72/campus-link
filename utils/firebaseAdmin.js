f3916dd6d51d3be0fdf17c6d770bc935c5739b4a
rinku sarkar <54875424+sarkr72@users.noreply.github.com>
2024-04-30 00:09:42 -0400

    Update firebaseAdmin.js

--- utils/firebaseAdmin.js
+++ utils/firebaseAdmin.js
@@ -1,13 +1,14 @@
-import admin from 'firebase-admin';
-//import key from '@/key';
-// const serviceAccount = require('../key');
+// import { app } from './firebase';
+// import admin from 'firebase-admin';
+// import key from '@/key';
+// // const serviceAccount = require('../key');
 
-// Initialize Firebase Admin SDK
-if (!admin.apps.length) {
-  //admin.initializeApp({
-   // credential: admin.credential.cert(key),
-    // databaseURL: 'https://your-project-id.firebaseio.com'
-  //});
-}
+// // Initialize Firebase Admin SDK
+// if (!admin.apps.length) {
+//   admin.initializeApp({
+//     credential: admin.credential.cert(key),
+//     // databaseURL: 'https://your-project-id.firebaseio.com'
+//   });
+// }
 
-export { admin };
+// export { admin };
